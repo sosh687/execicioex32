546c59ca5166f4f92137bba52846696610c926f0
sosh687 <sophiamoritadantas7@gmail.com>
2025-05-08 12:30:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function adicionarTarefa(){
+    const input = document.getElementById('nova-tarefa');
+    const textoTarefa = input.ariaValueMax.trim();
+}
+
+if (textoTarefa ==="") return;
+
+// paramos na linha 71 //
